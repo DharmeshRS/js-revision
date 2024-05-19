30da462dc0dcd9d88aae6a0fe50c6d843aa34092
Dharmesh Sonar <55545639+DharmeshRS@users.noreply.github.com>
2024-05-19 14:10:57 +0000

    add : datatypes added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 123456;
+let accountEmail = "dsonar333@gmail.com";
+var accountPassword = "dharmesh";
+accountCity = "Malegaon";
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*
+const we are using for declaring variable as constant in there scope
+let also have block scope
+var is global scope never use this. if we use var we can say its like bad practices 
+
+majorly use let and const
+
+*/
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // which helps to execute/treat older js code as newer version of js.
+
+
+
+// datatypes
+
+/*
+--> premitive datatypes
+number => 2 to power 53
+NOTE : upto 15 dogit js provides acuracy then integer field turns into bigint field. 
+bigint => 
+string => ""
+boolean => true/false
+null => standalone value
+undefined=> we defined the variable but not assigmed any value
+symbol => for uniqueness
+
+---> non-premitive datatypes
+
+
+
+
+*/
+
+
+console.log(undefined === null);
