1901f43a07a26048feac231088dd241618ad8eb1
Dharmesh Sonar <55545639+DharmeshRS@users.noreply.github.com>
2024-06-02 06:30:12 +0000

    revision : conversion if operations like typeof , NUmber, String

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+
+//Number
+
+const age = 33; 
+console.log("age",age);
+console.log("typeof",typeof age);
+
+// const ageInNumber = Number(age);
+
+// console.log("ageInNumber", ageInNumber);
+// console.log("type",typeof ageInNumber);
+
+const ageInString = String(age);
+console.log("ageInString",ageInString);
+console.log("type",typeof ageInString);
+
+
+
+
+
+
