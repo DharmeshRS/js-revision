6643d7b9a19870be5b98e2c8623b064cfee60557
Dharmesh Sonar <55545639+DharmeshRS@users.noreply.github.com>
2024-06-22 06:58:49 +0000

    revision related to numbers

---
+++ 01_basics/06_number.js
@@ -0,0 +1,14 @@
+const score = "100";
+const balance = new Number(score);
+console.log(balance)
+console.log(typeof balance)
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+
+console.log(0.55555555555566666666666666)
+console.log(new Number (0.55555555555566666666666666))
+// console.log(BigInt (0.55555555555566666))
+const bigIntNumber = parseFloat(0.55555555555566666);
+console.log(bigIntNumber);
